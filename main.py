d261a10ef30dcce6390a3e6920d6f6ef4d948e85
PC <rsaha008@fiu.edu>
2022-12-17 01:00:13 -0500

    main

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import Discord
 
-#pakora loves vaccuming
+import discord
+from discord.ext import commands
+
+TOKEN=''
+intents=discord.Intents.all()
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Bot is ready to use!')
+
+@bot.command(name='hello')
+async def msg(ctx):
+    if ctx.author == bot.user:
+        return
+    else:
+        await ctx.send("Hey!")
+
+bot.run(TOKEN)
