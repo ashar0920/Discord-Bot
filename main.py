cc816c42d524375731eea2612f9b2d2cfa521e53
Rohit <rsaha008@fiu.edu>
2022-12-18 22:27:23 -0500

    changed slash commands to work properly

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 TOKEN = 'MTA1MzU0MjM5MDY1NDMwODQyNA.GnQGs6.iptJpqgN-5IjKVLBuIYEb_3MlqQ2hPc7D9MM1w'
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(intents=intents)
 #bot = discord.Bot(intents=discord.Intents.discord())
 ##client = commands.Bot(command_prefix='!')
 
@@ -16,37 +16,37 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     print('Bot is ready to use!')
 
-@bot.command(name='hello')
+@bot.slash_command(name='hello')
 async def msg(ctx):
     if ctx.author == bot.user:
         return
     else:
         await ctx.send("Hey!")
 
-@bot.command(name='vacuum')
-async def msg(ctx):
-    if ctx.author == bot.user:
-        return
-    else: 
-        await ctx.send("ooh tasty <a:DonutLicker:1053598213262815232>") 
+# @bot.command(name='vacuum')
+# async def msg(ctx):
+#     if ctx.author == bot.user:
+#         return
+#     else: 
+#         await ctx.send("ooh tasty <a:DonutLicker:1053598213262815232>") 
 
-@bot.command(name='test')
-async def msg(ctx):
-    if ctx.author == bot.user:
-        return
-    else:
-        await ctx.send(":sob:")
+# @bot.command(name='test')
+# async def msg(ctx):
+#     if ctx.author == bot.user:
+#         return
+#     else:
+#         await ctx.send(":sob:")
 
-@bot.command(name='test1')
-async def msg(ctx):
-    if ctx.author == bot.user:
-        return
-    else:
-        await ctx.send("Hey!")
+# @bot.command(name='test1')
+# async def msg(ctx):
+#     if ctx.author == bot.user:
+#         return
+#     else:
+#         await ctx.send("Hey!")
         
 #peensize command
     
-@bot.command(name='peensize')
+@bot.slash_command(name='peensize')
 async def msg(ctx):
     peensize = random.randint(0,9)
     await ctx.send(f"your peen is {peensize} inches")
